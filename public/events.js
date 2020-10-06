8a4a5bc6de62793a2e58ed6824eb4ef8d806560e
maxwelljmckee <maxwelljmckee@gmail.com>
2020-10-06 12:15:11 -0600

    phase 4 complete

--- public/events.js
+++ public/events.js
@@ -21,3 +21,31 @@ document.getElementById("new-pic").addEventListener("click", async (event) => {
 		document.querySelector(".error").innerHTML = json.message;
 	}
 });
+
+// const updateScore = (response) => {
+//   if ()
+// }
+
+document.getElementById('upvote')
+.addEventListener('click', async (event) => {
+  const response = await fetch('/kitten/upvote', { method: 'PATCH' });
+  const json = await response.json();
+  try {
+    if (!response.ok) throw Error(json);
+    document.querySelector('.score').innerHTML = json.score;
+  } catch (error) {
+    document.querySelector(".error").innerHTML = json.message;
+  }
+})
+
+document.getElementById('downvote')
+.addEventListener('click', async (event) => {
+  const response = await fetch('/kitten/downvote', { method: 'PATCH' });
+  const json = await response.json();
+  try {
+    if (!response.ok) throw Error(json);
+    document.querySelector('.score').innerHTML = json.score;
+  } catch (error) {
+    document.querySelector(".error").innerHTML = json.message;
+  }
+})
