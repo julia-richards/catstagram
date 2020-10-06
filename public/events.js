32288cf1b857a80b6e81135f827a4b68bbdc7a8f
maxwelljmckee <maxwelljmckee@gmail.com>
2020-10-06 14:56:43 -0600

    wrinkly head cat

--- public/events.js
+++ public/events.js
@@ -1,3 +1,5 @@
+
+// LOAD CAT PHOTO ONLOAD //
 window.addEventListener("DOMContentLoaded", async (event) => {
 	const response = await fetch("/kitten/image");
 	const json = await response.json();
@@ -9,7 +11,10 @@ window.addEventListener("DOMContentLoaded", async (event) => {
 	}
 });
 
+
+// LOAD RANDOM CAT PHOTO ON NEW-PIC CLICK //
 document.getElementById("new-pic").addEventListener("click", async (event) => {
+  document.querySelector(".error").innerHTML = '';
 	document.querySelector(".loader").innerHTML = "LOADING...";
 	const response = await fetch("/kitten/image");
 	document.querySelector(".loader").innerHTML = "";
@@ -18,10 +23,13 @@ document.getElementById("new-pic").addEventListener("click", async (event) => {
 		if (!response.ok) throw Error(json);
 		document.querySelector(".cat-pic").src = json.src;
 	} catch (error) {
+		document.querySelector(".cat-pic").src = 'json.src';
 		document.querySelector(".error").innerHTML = json.message;
 	}
 });
 
+
+// INCREMENT LIKES //
 async function updateScore(response) {
 	const json = await response.json();
 	try {
@@ -32,6 +40,8 @@ async function updateScore(response) {
 	}
 }
 
+
+// DECREMENT LIKES //
 document.getElementById("upvote").addEventListener("click", async (event) => {
 	const response = await fetch("/kitten/upvote", { method: "PATCH" });
 	updateScore(response);
@@ -42,11 +52,13 @@ document.getElementById("downvote").addEventListener("click", async (event) => {
 	updateScore(response);
 });
 
+
+// HANDLE COMMENT SUBMISSION EVENT //
 document.getElementById("submit").addEventListener("click", async (event) => {
 	event.preventDefault();
   const form = new FormData(document.querySelector(".comment-form"));
   const comment = form.get("user-comment");
-	console.log(comment);
+	// console.log(comment);
 	const response = await fetch("/kitten/comments", {
 		method: "POST",
 		headers: {
@@ -57,7 +69,20 @@ document.getElementById("submit").addEventListener("click", async (event) => {
 	const json = await response.json();
 
 	try {
-		if (!response.ok) throw Error(json);
-		document.querySelector(".comments").innerHTML = json.comments.join('\n');
-	} catch (e) {}
+    if (!response.ok) throw Error(json);
+    const commentBlock = document.querySelector('.comments')
+
+    let comment = json.comments[json.comments.length - 1]
+    let currComm = document.createElement('p')
+    currComm.innerHTML = comment
+    commentBlock.appendChild(currComm)
+	} catch (e) {
+    let comment = document.createElement('p');
+    comment.innerHTML = 'something bad happened'
+    commentBlock.appendChild(comment)
+  }
 });
+
+
+
+// COMMENT DELETION //
