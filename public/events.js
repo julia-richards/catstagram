8f4e1104fbd7fe23d946e151d6a747d947cc1fd6
julia-richards <julia.m.richards@gmail.com>
2020-10-06 15:47:46 -0600

    worked on comment deletion function

--- public/events.js
+++ public/events.js
@@ -1,4 +1,3 @@
-
 // LOAD CAT PHOTO ONLOAD //
 window.addEventListener("DOMContentLoaded", async (event) => {
 	const response = await fetch("/kitten/image");
@@ -11,10 +10,9 @@ window.addEventListener("DOMContentLoaded", async (event) => {
 	}
 });
 
-
 // LOAD RANDOM CAT PHOTO ON NEW-PIC CLICK //
 document.getElementById("new-pic").addEventListener("click", async (event) => {
-  document.querySelector(".error").innerHTML = '';
+	document.querySelector(".error").innerHTML = "";
 	document.querySelector(".loader").innerHTML = "LOADING...";
 	const response = await fetch("/kitten/image");
 	document.querySelector(".loader").innerHTML = "";
@@ -23,11 +21,10 @@ document.getElementById("new-pic").addEventListener("click", async (event) => {
 		if (!response.ok) throw Error(json);
 		document.querySelector(".cat-pic").src = json.src;
 	} catch (error) {
-		document.querySelector(".cat-pic").src = 'json.src';
+		document.querySelector(".cat-pic").src = "json.src";
 		document.querySelector(".error").innerHTML = json.message;
 	}
 });
-
 
 // INCREMENT LIKES //
 async function updateScore(response) {
@@ -40,7 +37,6 @@ async function updateScore(response) {
 	}
 }
 
-
 // DECREMENT LIKES //
 document.getElementById("upvote").addEventListener("click", async (event) => {
 	const response = await fetch("/kitten/upvote", { method: "PATCH" });
@@ -52,37 +48,47 @@ document.getElementById("downvote").addEventListener("click", async (event) => {
 	updateScore(response);
 });
 
-
 // HANDLE COMMENT SUBMISSION EVENT //
 document.getElementById("submit").addEventListener("click", async (event) => {
 	event.preventDefault();
-  const form = new FormData(document.querySelector(".comment-form"));
-  const comment = form.get("user-comment");
-	// console.log(comment);
+	const form = new FormData(document.querySelector(".comment-form"));
+	const comment = form.get("user-comment");
 	const response = await fetch("/kitten/comments", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 		},
-		body: JSON.stringify({comment})
+		body: JSON.stringify({ comment }),
 	});
 	const json = await response.json();
 
 	try {
-    if (!response.ok) throw Error(json);
-    const commentBlock = document.querySelector('.comments')
+		if (!response.ok) throw Error(json);
+		const commentBlock = document.querySelector(".comments");
 
-    let comment = json.comments[json.comments.length - 1]
-    let currComm = document.createElement('p')
-    currComm.innerHTML = comment
-    commentBlock.appendChild(currComm)
+		let comment = json.comments[json.comments.length - 1];
+		let currComm = document.createElement("div");
+		let delButton = document.createElement("button");
+		delButton.setAttribute("class", "delete");
+		delButton.innerText = "delete comment";
+		currComm.innerHTML = comment + " ";
+		currComm.appendChild(delButton);
+		commentBlock.appendChild(currComm);
 	} catch (e) {
-    let comment = document.createElement('p');
-    comment.innerHTML = 'something bad happened'
-    commentBlock.appendChild(comment)
-  }
+		let comment = document.createElement("p");
+		comment.innerHTML = "something bad happened";
+		commentBlock.appendChild(comment);
+	}
 });
 
-
-
 // COMMENT DELETION //
+
+document.querySelector(".comments").addEventListener('click', async(event) =>{
+    if(event.target.tagName === "BUTTON"){
+      console.log(event.target.parentNode.innerText)
+      let response = await fetch('/kitten/comments/:id', {
+        method: "DELETE",
+        // body: JSON.stringify({event.target.parentNode.innerText})
+      });
+    }
+})
