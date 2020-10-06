835b02e1360063d0223f677cce13bc9c3657c7fc
julia-richards <julia.m.richards@gmail.com>
2020-10-06 12:02:48 -0600

    completed error handling

--- public/events.js
+++ public/events.js
@@ -1,16 +1,23 @@
-window.addEventListener('DOMContentLoaded', async (event) => {
-  const response = await fetch('/kitten/image')
-  const json = await response.json();
-  document.querySelector('.cat-pic').src = json.src
-  if (!response.ok) {} // update DOM w error message
+window.addEventListener("DOMContentLoaded", async (event) => {
+	const response = await fetch("/kitten/image");
+	const json = await response.json();
+	try {
+		if (!response.ok) throw Error(json);
+		document.querySelector(".cat-pic").src = json.src;
+	} catch (error) {
+		document.querySelector(".error").innerHTML = json.message;
+	}
 });
 
-document.getElementById('new-pic')
-.addEventListener('click', async (event) => {
-  document.querySelector('.loader').innerHTML = 'LOADING...';
-  const response = await fetch('/kitten/image');
-  document.querySelector('.loader').innerHTML = '';
-  const json = await response.json();
-  document.querySelector('.cat-pic').src = json.src
-  if (!response.ok) { } // update DOM w error message
-})
+document.getElementById("new-pic").addEventListener("click", async (event) => {
+	document.querySelector(".loader").innerHTML = "LOADING...";
+	const response = await fetch("/kitten/image");
+	document.querySelector(".loader").innerHTML = "";
+	const json = await response.json();
+	try {
+		if (!response.ok) throw Error(json);
+		document.querySelector(".cat-pic").src = json.src;
+	} catch (error) {
+		document.querySelector(".error").innerHTML = json.message;
+	}
+});
