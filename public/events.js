9e7dce785e5ede31cefc0aa49f24a888ee55fcfb
julia-richards <julia.m.richards@gmail.com>
2020-10-06 14:35:29 -0600

    updated comment form event listener

--- public/events.js
+++ public/events.js
@@ -22,30 +22,42 @@ document.getElementById("new-pic").addEventListener("click", async (event) => {
 	}
 });
 
-// const updateScore = (response) => {
-//   if ()
-// }
+async function updateScore(response) {
+	const json = await response.json();
+	try {
+		if (!response.ok) throw Error(json);
+		document.querySelector(".score").innerHTML = json.score;
+	} catch (error) {
+		document.querySelector(".error").innerHTML = json.message;
+	}
+}
 
-document.getElementById('upvote')
-.addEventListener('click', async (event) => {
-  const response = await fetch('/kitten/upvote', { method: 'PATCH' });
-  const json = await response.json();
-  try {
-    if (!response.ok) throw Error(json);
-    document.querySelector('.score').innerHTML = json.score;
-  } catch (error) {
-    document.querySelector(".error").innerHTML = json.message;
-  }
-})
+document.getElementById("upvote").addEventListener("click", async (event) => {
+	const response = await fetch("/kitten/upvote", { method: "PATCH" });
+	updateScore(response);
+});
 
-document.getElementById('downvote')
-.addEventListener('click', async (event) => {
-  const response = await fetch('/kitten/downvote', { method: 'PATCH' });
-  const json = await response.json();
-  try {
-    if (!response.ok) throw Error(json);
-    document.querySelector('.score').innerHTML = json.score;
-  } catch (error) {
-    document.querySelector(".error").innerHTML = json.message;
-  }
-})
+document.getElementById("downvote").addEventListener("click", async (event) => {
+	const response = await fetch("/kitten/downvote", { method: "PATCH" });
+	updateScore(response);
+});
+
+document.getElementById("submit").addEventListener("click", async (event) => {
+	event.preventDefault();
+  const form = new FormData(document.querySelector(".comment-form"));
+  const comment = form.get("user-comment");
+	console.log(comment);
+	const response = await fetch("/kitten/comments", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({comment})
+	});
+	const json = await response.json();
+
+	try {
+		if (!response.ok) throw Error(json);
+		document.querySelector(".comments").innerHTML = json.comments.join('\n');
+	} catch (e) {}
+});
